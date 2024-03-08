dfe0c3de2d2a5dbac4c69ac6c4084e3b7016a762
joshunrau <contact@joshuaunrau.com>
2024-03-08 15:52:14 -0500

    config

--- vitest.config.js
+++ vitest.config.js
@@ -26,6 +26,16 @@ export default defineConfig({
     })
   ],
   test: {
+    coverage: {
+      include: ['src/**/*'],
+      provider: 'v8',
+      // thresholds: {
+      //   branches: 100,
+      //   functions: 100,
+      //   lines: 100,
+      //   statements: 100
+      // }
+    },
     globals: true,
     root: './'
   }
