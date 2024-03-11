a41f002d21abf09db801dd769ac3addf5c2eb7c4
joshunrau <contact@joshuaunrau.com>
2024-03-11 16:40:09 -0400

    export ParseObjectIdPipe

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 export * from './decorators/current-user.decorator.js';
 export * from './decorators/validation-schema.decorator.js';
 
+export * from './pipes/parse-object-id.pipe.js';
 export * from './pipes/parse-schema.pipe.js';
 export * from './pipes/validation.pipe.js';
