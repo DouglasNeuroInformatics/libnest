0806e0ad067c497265e3ea6e6de155cfb76c272f
joshunrau <contact@joshuaunrau.com>
2024-07-19 09:37:50 -0400

    feat: add pbkdf2Params option to CryptoService

--- src/modules/crypto/crypto.config.ts
+++ src/modules/crypto/crypto.config.ts
@@ -1,6 +1,11 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common';
 
+type PBKDF2Params = {
+  iterations: number;
+};
+
 type CryptoModuleOptions = {
+  pbkdf2Params?: PBKDF2Params;
   secretKey: string;
 };
 
@@ -11,8 +16,5 @@ const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } = new ConfigurableModule
   }))
   .build();
 
-export {
-  ConfigurableModuleClass as ConfigurableCryptoModule,
-  type CryptoModuleOptions,
-  MODULE_OPTIONS_TOKEN as CRYPTO_MODULE_OPTIONS_TOKEN
-};
+export { ConfigurableModuleClass as ConfigurableCryptoModule, MODULE_OPTIONS_TOKEN as CRYPTO_MODULE_OPTIONS_TOKEN };
+export type { CryptoModuleOptions };
