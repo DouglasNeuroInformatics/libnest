1c4f7dd1859bafd0f06e9764a23e5ab020794dab
joshunrau <contact@joshuaunrau.com>
2024-11-25 16:10:27 -0500

    chore: update linting config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,17 @@
+import * as path from 'node:path';
+
 import { config } from '@douglasneuroinformatics/eslint-config';
 
-export default config();
+export default config([
+  {
+    env: {
+      browser: false,
+      es2021: true,
+      node: true
+    },
+    typescript: {
+      enabled: true,
+      project: path.resolve(import.meta.dirname, 'tsconfig.json')
+    }
+  }
+]);
