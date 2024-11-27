b38d7089fc42621cd4c8db604928ac3d1ddaffb8
joshunrau <contact@joshuaunrau.com>
2024-11-27 12:29:43 -0500

    feat: add exception handler

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export * from './decorators/current-user.decorator.js';
 export * from './decorators/validation-schema.decorator.js';
+export * from './exceptions/exception.handler.js';
 export * from './pipes/parse-schema.pipe.js';
 export * from './pipes/validation.pipe.js';
