5a1d4ccbfeaad1cc39c47e0819fb50949fa715e9
joshunrau <contact@joshuaunrau.com>
2025-01-25 12:07:55 -0500

    chore!: update to express v5

--- src/core/decorators/current-user.decorator.ts
+++ src/core/decorators/current-user.decorator.ts
@@ -1,5 +1,4 @@
 import { createParamDecorator } from '@nestjs/common';
-import type { ExecutionContext } from '@nestjs/common';
 
 import type { AppRequest } from '../types.js';
 
@@ -9,10 +8,7 @@ type CurrentUserDecorator = (key: Extract<keyof AppRequest['user'], string>) => 
  * Extract the user from the request object
  * @param key - the key of the user object to extract, or omit for the entire user
  */
-export const CurrentUser: CurrentUserDecorator = createParamDecorator<
-  Extract<keyof AppRequest['user'], string>,
-  ExecutionContext
->((key, context) => {
+export const CurrentUser: CurrentUserDecorator = createParamDecorator((key, context) => {
   const request = context.switchToHttp().getRequest<AppRequest>();
   if (key) {
     return request.user?.[key];
