3c4d4a9d6afda580a82034c295b9efdb476f9996
joshunrau <contact@joshuaunrau.com>
2025-01-27 12:00:34 -0500

    test: add

--- src/testing/mock/__tests__/mock.factory.spec.ts
+++ src/testing/mock/__tests__/mock.factory.spec.ts
@@ -1,13 +1,42 @@
-import { describe, expect, it } from 'vitest';
+import { Test } from '@nestjs/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
 
+import { ConfigService } from '../../../config/config.service.js';
+import { CryptoService } from '../../../crypto/crypto.service.js';
 import { getModelToken } from '../../../prisma/prisma.utils.js';
-import { MockFactory } from '../mock.factory.js';
+import { type MockedInstance, MockFactory } from '../mock.factory.js';
 
 describe('MockFactory', () => {
   describe('createForModel', () => {
     it('should provide the correct token', () => {
       expect(MockFactory.createForModel('User')).toMatchObject({
         provide: getModelToken('User')
+      });
+    });
+  });
+  describe('createForService', () => {
+    let configService: MockedInstance<ConfigService>;
+    let cryptoService: MockedInstance<CryptoService>;
+
+    beforeEach(async () => {
+      const moduleRef = await Test.createTestingModule({
+        providers: [MockFactory.createForService(ConfigService), MockFactory.createForService(CryptoService)]
+      }).compile();
+      configService = moduleRef.get(ConfigService);
+      cryptoService = moduleRef.get(CryptoService);
+    });
+
+    it('should successfully mock the ConfigService', () => {
+      expect(configService).toMatchObject({
+        get: expect.any(Function)
+      });
+    });
+
+    it('should successfully mock the CryptoService', () => {
+      expect(cryptoService).toMatchObject({
+        comparePassword: expect.any(Function),
+        hash: expect.any(Function),
+        hashPassword: expect.any(Function)
       });
     });
   });
