c5cd80a503d387adcd0af060248bc1a34ffe574d
joshunrau <contact@joshuaunrau.com>
2025-01-29 20:41:32 -0500

    feat: add docs

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 export * from './decorators/current-user.decorator.js';
 export * from './decorators/validation-schema.decorator.js';
+export * from './docs.js';
 export * from './exceptions/exception.handler.js';
 export * from './pipes/parse-schema.pipe.js';
 export * from './pipes/validation.pipe.js';
