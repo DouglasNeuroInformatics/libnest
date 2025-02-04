7eb910140627f30ade07c71aca8e9fd40a919732
joshunrau <contact@joshuaunrau.com>
2025-02-04 14:55:00 -0500

    refactor: add VALIDATION_SCHEMA_METADATA_KEY

--- src/core/decorators/validation-schema.decorator.ts
+++ src/core/decorators/validation-schema.decorator.ts
@@ -1,10 +1,12 @@
 import type { Class } from 'type-fest';
 import type { ZodType } from 'zod';
 
+export const VALIDATION_SCHEMA_METADATA_KEY = 'LIBNEST_VALIDATION_SCHEMA';
+
 /**
  * Decorator to define the zod validation schema for DTO classes */
 export function ValidationSchema<T>(schema: ZodType<T>) {
   return (target: Class<T>) => {
-    Reflect.defineMetadata('ValidationSchema', schema, target);
+    Reflect.defineMetadata(VALIDATION_SCHEMA_METADATA_KEY, schema, target);
   };
 }
