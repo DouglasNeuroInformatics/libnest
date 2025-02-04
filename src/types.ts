04081194d598e749fee6d8dedeb03f213053c928
joshunrau <contact@joshuaunrau.com>
2025-02-04 15:37:05 -0500

    refactor: augment request

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
 import type { PrismaClientLike } from './prisma/prisma.types.js';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+    interface User {
+      [key: string]: unknown;
+    }
+  }
+}
 
 export interface UserConfig {}
 
