61abf3559e5a89aa7c1d2a7a936d194773bc2912
joshunrau <contact@joshuaunrau.com>
2025-02-05 16:19:19 -0500

    test: add prisma decorators test

---
+++ src/prisma/__tests__/prisma.decorators.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { InjectModel } from '../prisma.decorators.js';
+
+import type { PrismaModelName } from '../prisma.types.js';
+
+const Inject = vi.hoisted(() => vi.fn(() => 'INJECTED'));
+const getModelToken = vi.hoisted(() => vi.fn((modelName: PrismaModelName) => `MockToken_${modelName}`));
+
+vi.mock('@nestjs/common', () => ({ Inject }));
+
+vi.mock('../prisma.utils.js', () => ({ getModelToken }));
+
+describe('InjectModel', () => {
+  it('should call Inject with the correct model token', () => {
+    const modelName = 'User';
+    const model = InjectModel(modelName);
+    expect(getModelToken).toHaveBeenCalledWith(modelName);
+    expect(Inject).toHaveBeenCalledWith(`MockToken_${modelName}`);
+    expect(model).toBe('INJECTED');
+  });
+});
