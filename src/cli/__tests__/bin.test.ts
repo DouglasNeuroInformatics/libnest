ccee844b7b9a6958ef543afe6fb98785264e7f4e
joshunrau <contact@joshuaunrau.com>
2025-02-08 15:01:40 -0500

    test: add bin test

---
+++ src/cli/__tests__/bin.test.ts
@@ -0,0 +1,17 @@
+import { spawnSync } from 'node:child_process';
+import * as path from 'node:path';
+
+import { describe, expect, it } from 'vitest';
+
+const program = path.resolve(import.meta.dirname, '../bin.js');
+
+describe('CLI', () => {
+  it('should successfully output help', () => {
+    const result = spawnSync(`node ${program} --help`, {
+      encoding: 'utf-8',
+      shell: true
+    });
+    expect(result.status).toBe(0);
+    expect(result.stdout).toContain('Usage: @douglasneuroinformatics/libnest');
+  });
+});
