ac14c839ddd3357597559a209cfa74e8acc356a0
joshunrau <contact@joshuaunrau.com>
2025-02-11 17:08:50 -0500

    docs: document config service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -6,12 +6,24 @@ import type { RuntimeConfig } from '../user-config.js';
 
 @Injectable()
 export class ConfigService {
+  /** @hidden */
   constructor(@Inject(CONFIG_TOKEN) private readonly config: RuntimeConfig) {}
 
+  /**
+   * Retrieves a configuration value by key.
+   * @param key The key of the configuration value to retrieve.
+   * @returns The configuration value
+   */
   get<TKey extends Extract<keyof RuntimeConfig, string>>(key: TKey): RuntimeConfig[TKey] {
     return this.config[key];
   }
 
+  /**
+   * Retrieves a configuration value by key, throwing an error if the value is undefined.
+   * @param key The key of the configuration value to retrieve.
+   * @returns The configuration value.
+   * @throws Error if the configuration value is undefined.
+   */
   getOrThrow<TKey extends Extract<keyof RuntimeConfig, string>>(key: TKey): Exclude<RuntimeConfig[TKey], undefined> {
     const value = this.config[key];
     if (value === undefined) {
