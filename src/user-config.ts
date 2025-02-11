83e7f74aa2281f0f4268f6d22e5808a2fe44fff0
joshunrau <contact@joshuaunrau.com>
2025-02-11 16:39:06 -0500

    docs: update

--- src/user-config.ts
+++ src/user-config.ts
@@ -1,3 +1,9 @@
+/**
+ * This module provides a helper function to setup for the `libnest` CLI. It also
+ * exports interfaces that users can augment in their application.
+ * @module user-config
+ */
+
 import type { BaseRuntimeConfig } from './config/config.schema.js';
 
 declare global {
@@ -11,17 +17,50 @@ declare global {
   }
 }
 
+/**
+ * Represents the parsed environment variables available at runtime.
+ *
+ * This type should be augmented by users to include additional configuration properties in their schema.
+ *
+ * ### Example:
+ * ```typescript
+ * declare module '@douglasneuroinformatics/libnest' {
+ *   export interface RuntimeConfig {
+ *     foo: string;
+ *   }
+ * }
+ * ```
+ */
 export interface RuntimeConfig extends BaseRuntimeConfig {}
 
+/**
+ * Represents the runtime `PrismaClient` for your application. Users should augment
+ * this declaration with their own `PrismaClient`.
+ */
+export type { PrismaClient } from './prisma/prisma.types.js';
+
+/**
+ * Configuration options for the user-defined settings in the `libnest` CLI.
+ */
 export interface UserConfigOptions {
+  /**
+   * The entry point for the `libnest` CLI.
+   *
+   * This should be a module whose default export is a function that initializes the application.
+   */
   entry: string;
-  globals?: {
-    [key: string]: unknown;
-  };
+
+  /**
+   * Optional global variables that should be defined at runtime.
+   */
+  globals?: { [key: string]: unknown };
 }
 
+/**
+ * Defines user configuration options with type safety.
+ * @param config - The configuration options for the application.
+ * @returns The same configuration options
+ */
 export function defineUserConfig(config: UserConfigOptions) {
   return config;
 }
-
-export type { PrismaClient } from './prisma/prisma.types.js';
