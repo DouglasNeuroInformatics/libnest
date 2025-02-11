170c7e25d739a3b30762eb40d0c90fc467799928
joshunrau <contact@joshuaunrau.com>
2025-02-11 17:07:23 -0500

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,9 +9,6 @@ export default config(
       es2021: true,
       node: true
     },
-    jsdoc: {
-      enabled: true
-    },
     typescript: {
       enabled: true,
       project: path.resolve(import.meta.dirname, 'tsconfig.json')
@@ -19,8 +16,8 @@ export default config(
   },
   {
     rules: {
-      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-namespace': 'off'
     }
   }
