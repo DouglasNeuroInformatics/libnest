e42cd4c19638156ec632d409211a3973f671be41
joshunrau <contact@joshuaunrau.com>
2025-02-11 16:26:01 -0500

    chore: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,20 @@ import * as path from 'node:path';
 import { config } from '@douglasneuroinformatics/eslint-config';
 
 export default config(
-  [
-    {
-      env: {
-        browser: false,
-        es2021: true,
-        node: true
-      },
-      typescript: {
-        enabled: true,
-        project: path.resolve(import.meta.dirname, 'tsconfig.json')
-      }
+  {
+    env: {
+      browser: false,
+      es2021: true,
+      node: true
+    },
+    jsdoc: {
+      enabled: true
+    },
+    typescript: {
+      enabled: true,
+      project: path.resolve(import.meta.dirname, 'tsconfig.json')
     }
-  ],
+  },
   {
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
