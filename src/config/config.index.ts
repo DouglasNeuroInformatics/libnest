0eb5429c0f1e70b666fc7b5ccdcbbe2a9d6614c0
joshunrau <contact@joshuaunrau.com>
2025-02-11 16:32:31 -0500

    chore: format

--- src/config/config.index.ts
+++ src/config/config.index.ts
@@ -1,7 +1,6 @@
 /**
  * This module provides a global service for reading environment variables and defines the
  * base Zod runtime schema required by all libnest applications at runtime.
- *
  * @module Config
  */
 
