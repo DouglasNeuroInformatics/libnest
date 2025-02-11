c40009a748fce8d25811c369b5b46c945676e1a1
joshunrau <contact@joshuaunrau.com>
2025-02-11 16:48:12 -0500

    docs: update

--- src/core/middleware/delay.middleware.ts
+++ src/core/middleware/delay.middleware.ts
@@ -4,6 +4,12 @@ export type DelayOptions = {
   responseDelay: number;
 };
 
+/**
+ * Middleware to introduce an arbitrary delay in the response, which can be useful for testing purposes.
+ * @param options - configuration options
+ * @param options.responseDelay - the length of delay in milliseconds
+ * @returns An Express middleware function.
+ */
 export function delay({ responseDelay }: DelayOptions) {
   return (_req: Request, _res: Response, next: NextFunction) => {
     setTimeout(() => {
