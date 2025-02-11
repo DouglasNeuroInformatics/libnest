4930a3f54101c0849b5e4663d8f89458d2af8e34
joshunrau <contact@joshuaunrau.com>
2025-02-11 17:00:27 -0500

    refactor: use overloads for dto

--- src/core/mixins/data-transfer-object.mixin.ts
+++ src/core/mixins/data-transfer-object.mixin.ts
@@ -3,15 +3,26 @@ import { z } from 'zod';
 
 import { applyValidationSchema } from '../decorators/validation-schema.decorator.js';
 
-function createDto<T extends z.ZodRawShape>(shape: T): Class<z.TypeOf<z.ZodObject<T>>> {
-  const schema = z.object(shape);
+/**
+ * Creates a Data Transfer Object (DTO) class with a Zod schema for validation.
+ * @param schema - Zod object schema for the DTO.
+ * @returns A DTO class with validation.
+ */
+export function DataTransferObject<T extends z.ZodType<{ [key: string]: any }>>(schema: T): Class<z.TypeOf<T>>;
+/**
+ * Creates a Data Transfer Object (DTO) class with a Zod schema for validation.
+ * @param shapeOrSchema - Zod raw shape for the DTO.
+ * @returns A DTO class with validation.
+ */
+export function DataTransferObject<T extends z.ZodRawShape>(shapeOrSchema: T): Class<z.TypeOf<z.ZodObject<T>>>;
+/**
+ * Creates a Data Transfer Object (DTO) class with a Zod schema for validation.
+ * @param shapeOrSchema - Zod raw shape or schema for the DTO.
+ * @returns A DTO class with validation.
+ */
+export function DataTransferObject(shapeOrSchema: z.ZodRawShape | z.ZodType<{ [key: string]: any }>) {
+  const schema = shapeOrSchema instanceof z.ZodType ? shapeOrSchema : z.object(shapeOrSchema);
   const Target = class {} as Class<z.TypeOf<typeof schema>>;
   applyValidationSchema(Target, schema);
   return Target;
 }
-
-export const DataTransferObject = Object.assign(createDto, {
-  forSchema<T extends z.ZodRawShape>(this: typeof createDto, schema: z.ZodObject<T>) {
-    return this(schema.shape);
-  }
-});
