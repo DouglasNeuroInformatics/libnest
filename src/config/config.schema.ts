c1acdba03517df35ec1ea1e44ea511aef4f785af
joshunrau <contact@joshuaunrau.com>
2025-02-13 16:17:04 -0500

    refactor: use schema factories from libjs

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -1,4 +1,4 @@
-import { isNumberLike, parseNumber } from '@douglasneuroinformatics/libjs';
+import { $$BooleanLike, $$NumberLike } from '@douglasneuroinformatics/libjs';
 import { z } from 'zod';
 
 /**
@@ -11,26 +11,16 @@ import { z } from 'zod';
  * @see {@link user-config!RuntimeConfig | RuntimeConfig}
  */
 export const $BaseRuntimeConfig = z.object({
-  API_DEV_SERVER_PORT: z
-    .string()
-    .optional()
-    .transform((arg) => (isNumberLike(arg) ? parseNumber(arg) : arg))
-    .pipe(z.number().int().nonnegative()),
-  DEBUG: z
-    .enum(['true', 'false'])
-    .optional()
-    .transform((arg) => arg === 'true'),
-  MONGO_DIRECT_CONNECTION: z.enum(['true', 'false']).optional(),
+  API_DEV_SERVER_PORT: $$NumberLike((base) => base.int().nonnegative()),
+  DEBUG: $$BooleanLike().optional(),
+  MONGO_DIRECT_CONNECTION: $$BooleanLike().optional(),
   MONGO_REPLICA_SET: z.enum(['rs0']).optional(),
-  MONGO_RETRY_WRITES: z.enum(['true', 'false']).optional(),
+  MONGO_RETRY_WRITES: $$BooleanLike().optional(),
   MONGO_URI: z.string().url(),
   MONGO_WRITE_CONCERN: z.enum(['majority']).optional(),
   NODE_ENV: z.enum(['development', 'production', 'test']),
   SECRET_KEY: z.string().min(16),
-  VERBOSE: z
-    .enum(['true', 'false'])
-    .optional()
-    .transform((arg) => arg === 'true')
+  VERBOSE: $$BooleanLike().optional()
 });
 
 /**
