1a0934f094d3fe99848273b4d827f79ab8b6bd30
joshunrau <contact@joshuaunrau.com>
2025-02-14 13:39:03 -0500

    refactor: add other providers

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -1,8 +1,13 @@
 import type { DynamicModule, MiddlewareConsumer, ModuleMetadata, NestModule, Provider } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
+import { GlobalExceptionFilter } from '../filters/global-exception.filter.js';
 import { JSONLogger } from '../logging/json.logger.js';
 import { LOGGING_OPTIONS_TOKEN } from '../logging/logging.config.js';
 import { LoggingMiddleware } from '../logging/logging.middleware.js';
+import { LoggingService } from '../logging/logging.service.js';
+import { ValidationPipe } from '../pipes/validation.pipe.js';
 import { ConfigService } from '../services/config.service.js';
 import { CryptoService } from '../services/crypto.service.js';
 
@@ -18,34 +23,71 @@ export type CreateAppModuleOptions = {
 
 export class AppModule implements NestModule {
   static create({ config, imports, providers }: CreateAppModuleOptions): DynamicModule {
-    return {
-      imports,
-      module: AppModule,
-      providers: [
-        {
-          provide: ConfigService,
-          useValue: new ConfigService(config)
-        },
-        {
-          provide: CryptoService,
-          useValue: new CryptoService({
-            pbkdf2Params: {
-              iterations: config.DANGEROUSLY_DISABLE_PBKDF2_ITERATION ? 1 : 100_000
-            },
-            secretKey: config.SECRET_KEY
-          })
-        },
-        {
-          provide: LOGGING_OPTIONS_TOKEN,
-          useValue: {
-            debug: config.DEBUG,
-            log: config.NODE_ENV !== 'test',
-            verbose: config.VERBOSE
+    const coreImports: ImportedModule[] = [];
+    const coreProviders: Provider[] = [
+      {
+        provide: ConfigService,
+        useValue: new ConfigService(config)
+      },
+      {
+        provide: CryptoService,
+        useValue: new CryptoService({
+          pbkdf2Params: {
+            iterations: config.DANGEROUSLY_DISABLE_PBKDF2_ITERATION ? 1 : 100_000
+          },
+          secretKey: config.SECRET_KEY
+        })
+      },
+      {
+        provide: LOGGING_OPTIONS_TOKEN,
+        useValue: {
+          debug: config.DEBUG,
+          log: config.NODE_ENV !== 'test',
+          verbose: config.VERBOSE
+        }
+      },
+      JSONLogger,
+      LoggingService,
+      {
+        provide: APP_FILTER,
+        useClass: GlobalExceptionFilter
+      },
+      {
+        provide: APP_PIPE,
+        useClass: ValidationPipe
+      }
+    ];
+
+    if (config.THROTTLER_ENABLED) {
+      coreImports.push(
+        ThrottlerModule.forRoot([
+          {
+            limit: 25,
+            name: 'short',
+            ttl: 1000
+          },
+          {
+            limit: 100,
+            name: 'medium',
+            ttl: 10000
+          },
+          {
+            limit: 250,
+            name: 'long',
+            ttl: 60000
           }
-        },
-        JSONLogger,
-        ...providers
-      ]
+        ])
+      );
+      coreProviders.push({
+        provide: APP_GUARD,
+        useClass: ThrottlerGuard
+      });
+    }
+
+    return {
+      imports: [...coreImports, ...imports],
+      module: AppModule,
+      providers: [...coreProviders, ...providers]
     };
   }
   configure(consumer: MiddlewareConsumer) {
