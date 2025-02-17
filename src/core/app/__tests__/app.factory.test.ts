51e56552543014f09a391235377b306a1ff92fc9
joshunrau <contact@joshuaunrau.com>
2025-02-17 13:36:56 -0500

    test: add basic app factory test back

---
+++ src/core/app/__tests__/app.factory.test.ts
@@ -0,0 +1,42 @@
+import { beforeAll, describe, expect, it, vi } from 'vitest';
+
+import { $BaseEnv } from '../../../config/schema.js';
+import { AppFactory } from '../app.factory.js';
+import { CatsModule } from './stubs/cats.module.js';
+
+import type { BaseEnv } from '../../../config/schema.js';
+import type { CreateAppOptions } from '../app.factory.js';
+
+const env = {
+  API_DEV_SERVER_PORT: '5500',
+  DEBUG: 'false',
+  MONGO_URI: 'mongodb://localhost:27017',
+  NODE_ENV: 'test',
+  SECRET_KEY: '2622d72669dd194b98cffd9098b0d04b',
+  VERBOSE: 'false'
+} satisfies { [K in keyof BaseEnv]?: string };
+
+describe('AppFactory', () => {
+  describe('create', () => {
+    const createApp = (options?: Partial<CreateAppOptions>) => {
+      return AppFactory.create({
+        imports: [CatsModule],
+        schema: $BaseEnv,
+        version: '1',
+        ...options
+      });
+    };
+
+    beforeAll(() => {
+      Object.entries(env).forEach(([key, value]) => {
+        vi.stubEnv(key, value);
+      });
+    });
+
+    it('should throw an error if it cannot parse the schema', () => {
+      vi.stubEnv('VERBOSE', '1');
+      expect(() => createApp()).toThrow('Failed to Parse Environment Variables');
+      vi.stubEnv('VERBOSE', env.VERBOSE);
+    });
+  });
+});
