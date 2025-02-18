b2327c570d674a57f527935920c06694ba52e84f
joshunrau <contact@joshuaunrau.com>
2025-02-18 09:39:07 -0500

    test: update

--- src/core/mixins/__tests__/app-error-class.mixin.test.ts
+++ src/core/mixins/__tests__/app-error-class.mixin.test.ts
@@ -1,15 +1,16 @@
-import { beforeAll, describe, expect, it } from 'vitest';
+import { describe, expect, expectTypeOf, it } from 'vitest';
 
 import { AppErrorClass, BaseAppError } from '../app-error-class.mixin.js';
 
-describe('AppErrorClass', () => {
-  let TestError: typeof BaseAppError;
-
-  beforeAll(() => {
-    TestError = AppErrorClass('TestError');
+describe('BaseAppError', () => {
+  it('should have parameters matching the base error constructor by default', () => {
+    expectTypeOf<ConstructorParameters<typeof BaseAppError>>().toEqualTypeOf<ConstructorParameters<typeof Error>>();
   });
+});
 
+describe('AppErrorClass', () => {
   it('should create an instance of error with the provided name and message', () => {
+    const TestError = AppErrorClass('TestError');
     const error = new TestError('This is a test');
     expect(error).toBeInstanceOf(Error);
     expect(error.message).toBe('This is a test');
@@ -17,10 +18,16 @@ describe('AppErrorClass', () => {
   });
 
   it('should create distinct error constructors', () => {
+    const TestError = AppErrorClass('TestError');
     const OtherError = AppErrorClass('OtherError');
     const e1 = new TestError('This is a test');
     const e2 = new OtherError('This is a test');
     expect(e1).not.toBeInstanceOf(OtherError);
     expect(e2).not.toBeInstanceOf(TestError);
+  });
+
+  it('should have parameters assignable to those of the base error constructor by default', () => {
+    const _TestError = AppErrorClass('TestError');
+    expectTypeOf<ConstructorParameters<typeof _TestError>>().toMatchTypeOf<ConstructorParameters<typeof Error>>();
   });
 });
--- src/core/mixins/app-error-class.mixin.ts
+++ src/core/mixins/app-error-class.mixin.ts
@@ -1,13 +1,21 @@
+import type { Simplify } from 'type-fest';
+
+export type AppErrorOptions<TDetails extends { [key: string]: unknown }> = Simplify<
+  ErrorOptions & {
+    details?: TDetails;
+  }
+>;
+
 export class BaseAppError extends Error {
-  constructor(message: string) {
-    super(message);
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
   }
 }
 
-export function AppErrorClass(name: `${string}Error`) {
+export function AppErrorClass<TDetails extends { [key: string]: unknown }>(name: `${string}Error`) {
   return class extends BaseAppError {
-    constructor(message: string) {
-      super(message);
+    constructor(message: string, options?: AppErrorOptions<TDetails>) {
+      super(message, options);
       this.name = name;
     }
   };
