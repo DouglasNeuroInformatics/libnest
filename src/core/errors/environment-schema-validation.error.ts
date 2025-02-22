4d3cc5911ea2ae4b455e2ba5270c835d924e8749
joshunrau <contact@joshuaunrau.com>
2025-02-22 14:28:56 -0500

    refactor: update

--- src/core/errors/environment-schema-validation.error.ts
+++ src/core/errors/environment-schema-validation.error.ts
@@ -3,9 +3,6 @@ import { z } from 'zod';
 import { AppErrorClass } from '../mixins/app-error-class.mixin.js';
 
 export const EnvironmentSchemaValidationError = AppErrorClass({
-  extendType(infer) {
-    return infer<{ details: { issues: z.ZodIssue[] } }>();
-  },
   message: 'Failed to Parse Environment Variables',
   name: 'EnvironmentSchemaValidationError'
-});
+}).extendType<{ details: { issues: z.ZodIssue[] } }>();
