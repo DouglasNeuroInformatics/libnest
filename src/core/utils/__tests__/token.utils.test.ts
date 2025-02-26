04a840f05fc579720ec0806f8993483a0c228837
joshunrau <contact@joshuaunrau.com>
2025-02-26 14:05:21 -0500

    refactor: token

--- src/core/utils/__tests__/token.utils.test.ts
+++ src/core/utils/__tests__/token.utils.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { defineToken } from '../token.utils.js';
 
 describe('defineToken', () => {
-  it('should return an object with a single key where the key is equal to the value', () => {
-    expect(defineToken('LIBNEST_TOKEN')).toStrictEqual({ LIBNEST_TOKEN: 'LIBNEST_TOKEN' });
+  it('should return an object with a single key where the key is equal to the value, with a prefix attached', () => {
+    expect(defineToken('TOKEN')).toStrictEqual({ TOKEN: 'LIBNEST_TOKEN' });
   });
 });
