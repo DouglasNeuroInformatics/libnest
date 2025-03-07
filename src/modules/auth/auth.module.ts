c3045b1caa94445340760cadbb3c2ffdc31c43ac
joshunrau <contact@joshuaunrau.com>
2025-03-07 16:54:10 -0500

    chore: implement userQuery

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
+import { InternalServerErrorException, Module } from '@nestjs/common';
 import type { DynamicModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { ConfigService } from '../config/config.service.js';
+import { JSONLogger } from '../logging/json.logger.js';
 import { getModelToken } from '../prisma/prisma.utils.js';
 import { USER_QUERY_TOKEN } from './auth.config.js';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 
 import type { UserModelName } from '../prisma/prisma.types.js';
-import type { AuthOptions } from './auth.config.js';
+import type { AuthOptions, UserQuery } from './auth.config.js';
 
 @Module({})
 export class AuthModule {
@@ -34,8 +35,34 @@ export class AuthModule {
         {
           inject: [getModelToken(options.userModel)],
           provide: USER_QUERY_TOKEN,
-          useFactory: (userModel: { findFirst: () => (where: { username: string }) => unknown }) => {
-            return userModel;
+          useFactory: (userModel: {
+            findFirst: (args: { where: { username: string } }) => Promise<null | { [key: string]: unknown }>;
+          }): UserQuery => {
+            const logger = new JSONLogger('UserQuery');
+            return async (username: string) => {
+              const user = await userModel.findFirst({ where: { username } });
+              if (!user) {
+                return null;
+              }
+
+              const hashedPassword = user.hashedPassword;
+              if (typeof hashedPassword !== 'string') {
+                logger.error(`Unexpected type of property 'hashedPassword': ${typeof hashedPassword}`);
+                throw new InternalServerErrorException();
+              }
+
+              const tokenPayload: { [key: string]: unknown } = {};
+              options.tokenPayload.forEach((key) => {
+                const value = user[key];
+                if (value === undefined) {
+                  logger.error(`Cannot create token payload: property '${key}' on user is not defined`);
+                  throw new InternalServerErrorException();
+                }
+                tokenPayload[key] = value;
+              });
+
+              return { hashedPassword, tokenPayload };
+            };
           }
         },
         AuthService
