9deecbcf99641663a3afe4eab5d9bae2b86e9701
joshunrau <contact@joshuaunrau.com>
2025-03-09 16:12:48 -0400

    test: add auth module integration test

---
+++ src/modules/auth/__tests__/auth.module.spec.ts
@@ -0,0 +1,97 @@
+import type { IncomingMessage, Server, ServerResponse } from 'node:http';
+
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
+import type { Mock } from 'vitest';
+import { z } from 'zod';
+
+import { ConfigModule } from '../../config/config.module.js';
+import { CryptoModule } from '../../crypto/crypto.module.js';
+import { CryptoService } from '../../crypto/crypto.service.js';
+import { AuthModule } from '../auth.module.js';
+
+import type { BaseEnv } from '../../../schemas/env.schema.js';
+
+describe('AuthModule', () => {
+  let app: NestExpressApplication;
+  let server: Server<typeof IncomingMessage, typeof ServerResponse>;
+
+  let userQuery: Mock;
+
+  const loginCredentials = {
+    password: 'password',
+    username: 'admin'
+  };
+
+  // let cryptoService: CryptoService;
+  let hashedPassword: string;
+
+  beforeAll(async () => {
+    userQuery = vi.fn();
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AuthModule.forRoot({
+          loginCredentialsSchema: z.object({
+            password: z.string(),
+            username: z.string()
+          }),
+          userQuery
+        }),
+        ConfigModule.forRoot({
+          envConfig: {
+            SECRET_KEY: '12345678'
+          } satisfies Partial<BaseEnv> as BaseEnv
+        }),
+        CryptoModule.forRoot()
+      ]
+    }).compile();
+
+    const cryptoService = moduleRef.get(CryptoService);
+    hashedPassword = await cryptoService.hashPassword(loginCredentials.password);
+
+    app = moduleRef.createNestApplication({
+      logger: ['debug', 'error', 'fatal', 'log', 'verbose', 'warn']
+    });
+    await app.init();
+    server = app.getHttpServer();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+      app.flushLogs();
+    }
+  });
+
+  it('should return status code 401 if the user query returns null', async () => {
+    userQuery.mockResolvedValueOnce(null);
+    const response = await request(server).post('/auth/login').send(loginCredentials);
+    expect(response.status).toBe(401);
+    expect(userQuery).toHaveBeenCalledExactlyOnceWith(loginCredentials);
+  });
+
+  it('should return status code 401 if the hashed password is incorrect', async () => {
+    const comparePassword = vi.spyOn(CryptoService.prototype, 'comparePassword');
+    userQuery.mockResolvedValueOnce({ hashedPassword: '123$123' });
+    const response = await request(server).post('/auth/login').send(loginCredentials);
+    expect(response.status).toBe(401);
+    expect(userQuery).toHaveBeenCalledExactlyOnceWith(loginCredentials);
+    expect(comparePassword).toHaveBeenCalledExactlyOnceWith(loginCredentials.password, '123$123');
+  });
+
+  it('should return status code 200 and an access token if the credentials are correct', async () => {
+    userQuery.mockResolvedValueOnce({ hashedPassword, tokenPayload: {} });
+    const response = await request(server).post('/auth/login').send(loginCredentials);
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      accessToken: expect.stringMatching(/^[A-Za-z0-9-_]+\.([A-Za-z0-9-_]+)\.[A-Za-z0-9-_]+$/)
+    });
+  });
+});
