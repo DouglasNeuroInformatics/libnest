0f5b0b2c5c5505b749e3f45c46e00fdacea501d5
joshunrau <contact@joshuaunrau.com>
2025-03-10 19:50:37 -0400

    test: add first ability factory test

---
+++ src/modules/auth/__tests__/ability.factory.spec.ts
@@ -0,0 +1,31 @@
+import { Test } from '@nestjs/testing';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import type { Mock } from 'vitest';
+
+import { AbilityFactory } from '../ability.factory.js';
+import { APPLY_PERMISSIONS_TOKEN } from '../auth.config.js';
+
+describe('AbilityFactory', () => {
+  let abilityFactory: AbilityFactory;
+  let applyPermissions: Mock;
+
+  beforeEach(async () => {
+    applyPermissions = vi.fn();
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        AbilityFactory,
+        {
+          provide: APPLY_PERMISSIONS_TOKEN,
+          useValue: applyPermissions
+        }
+      ]
+    }).compile();
+    abilityFactory = moduleRef.get(AbilityFactory);
+  });
+
+  describe('createForUser', () => {
+    it('should return an empty ruleset, if applyPermissions is undefined', () => {
+      expect(abilityFactory.createForUser().rules).toStrictEqual([]);
+    });
+  });
+});
--- src/modules/auth/ability.factory.ts
+++ src/modules/auth/ability.factory.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { AbilityBuilder, detectSubjectType } from '@casl/ability';
+import { createPrismaAbility } from '@casl/prisma';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { APPLY_PERMISSIONS_TOKEN } from './auth.config.js';
+
+import type { AbilityModifier, AppAbility } from './auth.config.js';
 
 @Injectable()
-export class AbilityFactory {}
+export class AbilityFactory {
+  constructor(@Inject(APPLY_PERMISSIONS_TOKEN) private readonly applyPermissions?: AbilityModifier) {}
+
+  createForUser() {
+    const abilityBuilder = new AbilityBuilder<AppAbility>(createPrismaAbility);
+    if (this.applyPermissions) {
+      this.applyPermissions(abilityBuilder);
+    }
+    return abilityBuilder.build({
+      detectSubjectType: (obj: { [key: string]: unknown }) => {
+        if (typeof obj.__modelName === 'string') {
+          return obj.__modelName;
+        }
+        return detectSubjectType(obj);
+      }
+    });
+  }
+}
