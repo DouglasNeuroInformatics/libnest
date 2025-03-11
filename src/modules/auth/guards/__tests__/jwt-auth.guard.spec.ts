9c9d388b2d54933925021b2acd2904a15a32dd81
joshunrau <contact@joshuaunrau.com>
2025-03-11 15:25:26 -0400

    test: update

--- src/modules/auth/guards/__tests__/jwt-auth.guard.spec.ts
+++ src/modules/auth/guards/__tests__/jwt-auth.guard.spec.ts
@@ -99,6 +99,23 @@ describe('JwtAuthGuard', () => {
     await expect(guard.canActivate(context)).resolves.toBe(false);
   });
 
+  it('should return false for a protected route, if the user has only some of the right permissions', async () => {
+    reflector.get.mockReturnValueOnce([
+      {
+        action: 'read',
+        subject: 'Cat'
+      },
+      {
+        action: 'update',
+        subject: 'Cat'
+      }
+    ] satisfies RouteAccessType);
+    BaseConstructor.prototype.canActivate.mockResolvedValueOnce(true);
+    const ability = AbilityFactory.prototype.createForPermissions([{ action: 'read', subject: 'Cat' }]);
+    getRequest.mockReturnValueOnce({ url: 'http://localhost:5500', user: { ability } });
+    await expect(guard.canActivate(context)).resolves.toBe(false);
+  });
+
   it('should return true for a protected route, if the user has the right permissions', async () => {
     reflector.get.mockReturnValueOnce({
       action: 'manage',
