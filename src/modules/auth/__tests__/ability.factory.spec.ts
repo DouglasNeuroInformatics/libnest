62ab75f64794d51272a97238c1e2eb0435ecb669
joshunrau <contact@joshuaunrau.com>
2025-03-11 14:05:54 -0400

    test: update

--- src/modules/auth/__tests__/ability.factory.spec.ts
+++ src/modules/auth/__tests__/ability.factory.spec.ts
@@ -5,7 +5,7 @@ import type { Mock } from 'vitest';
 import { AbilityFactory } from '../ability.factory.js';
 import { DEFINE_ABILITY_TOKEN } from '../auth.config.js';
 
-import type { DefineAbility } from '../auth.config.js';
+import type { DefineAbility, Permission } from '../auth.config.js';
 
 describe('AbilityFactory', () => {
   let abilityFactory: AbilityFactory;
@@ -41,6 +41,7 @@ describe('AbilityFactory', () => {
           subject: 'all'
         }
       ]);
+      expect(ability.can('create', 'Cat')).toBe(true);
     });
     it('should correctly detect the subject type', () => {
       defineAbility.mockImplementationOnce((ability) => {
@@ -67,5 +68,43 @@ describe('AbilityFactory', () => {
       expect(ability.can('manage', dog)).toBe(false);
       expect(ability.can('manage', obj as any)).toBe(false);
     });
+  });
+
+  describe('createForPermissions', () => {
+    it('should create an ability for an empty permissions set', () => {
+      const permissions: Permission[] = [];
+      const ability = abilityFactory.createForPermissions(permissions);
+      expect(ability.rules).toStrictEqual(permissions);
+    });
+    it('should create an ability for a simple ruleset', () => {
+      const permissions: Permission[] = [{ action: 'manage', subject: 'all' }];
+      const ability = abilityFactory.createForPermissions(permissions);
+      expect(ability.rules).toStrictEqual(permissions);
+      expect(ability.can('create', 'Cat')).toBe(true);
+    });
+    // it('should correctly detect the subject type', () => {
+    //   const permissions: Permission[] = [
+    //     {
+    //       action: 'manage',
+    //       conditions: {
+    //         id: {
+    //           in: [0, 1]
+    //         }
+    //       },
+    //       subject: 'Cat'
+    //     }
+    //   ];
+    //   const ability = abilityFactory.createForPermissions(permissions);
+    //   expect(ability.rules).toStrictEqual(permissions);
+
+    //   // const cat1 = { __modelName: 'Cat', id: 1 };
+    //   // const cat2 = { __modelName: 'Cat', id: 2 };
+    //   // const dog = { __modelName: 'Dog', id: 1 };
+    //   // const obj = { id: 1 };
+    //   // expect(ability.can('manage', cat1)).toBe(true);
+    //   // expect(ability.can('manage', cat2)).toBe(false);
+    //   // expect(ability.can('manage', dog)).toBe(false);
+    //   // expect(ability.can('manage', obj as any)).toBe(false);
+    // });
   });
 });
