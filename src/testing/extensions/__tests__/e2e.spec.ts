0c7af21011c6b0df592815741aab14cb689b7542
joshunrau <contact@joshuaunrau.com>
2025-03-16 16:06:00 -0400

    test: update

--- src/testing/extensions/__tests__/e2e.spec.ts
+++ src/testing/extensions/__tests__/e2e.spec.ts
@@ -3,10 +3,16 @@ import { describe, expect, it } from 'vitest';
 
 import { e2e } from '../e2e.js';
 
-e2e('e2e (self)', ({ app }) => {
+e2e('e2e (self)', ({ api, app }) => {
   describe('app', () => {
     it('should be an instance of NestApplication', () => {
       expect(app).toBeInstanceOf(NestApplication);
+    });
+  });
+  describe('api', () => {
+    it('should allow sending a get request for the documentation', async () => {
+      const response = await api.get('/spec.json');
+      expect(response.status).toBe(200);
     });
   });
 });
