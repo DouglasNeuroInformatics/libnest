3230cd859a2ad4b656f7dc931fa871cc299568eb
joshunrau <contact@joshuaunrau.com>
2025-03-16 15:52:21 -0400

    refactor: use dynamic import is e2e

--- src/testing/e2e.ts
+++ src/testing/e2e.ts
@@ -3,8 +3,6 @@ import request from 'supertest';
 import type TestAgent from 'supertest/lib/agent.js';
 import { test } from 'vitest';
 
-import { findConfig, loadAppContainer, loadConfig } from '../utils/meta.utils.js';
-
 export type EndToEndFixtures = {
   api: TestAgent;
   app: NestExpressApplication;
@@ -20,6 +18,9 @@ export const e2e = test.extend<EndToEndFixtures>({
   app: [
     // eslint-disable-next-line no-empty-pattern
     async ({}, use): Promise<void> => {
+      // we need to use dynamic import here, as importing meta utils will also import AppContainer, which
+      // prevents mocking a host of dependencies in unit tests
+      const { findConfig, loadAppContainer, loadConfig } = await import('../utils/meta.utils.js');
       await findConfig(import.meta.dirname)
         .asyncAndThen(loadConfig)
         .andThen(loadAppContainer)
