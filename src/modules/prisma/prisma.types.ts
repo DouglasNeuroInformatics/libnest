3fccbcb30952d73468c17bfc5f9f7c64bbcb0d22
joshunrau <contact@joshuaunrau.com>
2025-03-16 12:06:01 -0400

    refactor: use ExtendedPrismaClient instead of RuntimePrismaClient

--- src/modules/prisma/prisma.types.ts
+++ src/modules/prisma/prisma.types.ts
@@ -1,14 +1,12 @@
 import type { SingleKeyMap } from '@douglasneuroinformatics/libjs';
 import type { Prisma, PrismaClient } from '@prisma/client';
-import type { IfNever, Simplify } from 'type-fest';
+import type { IfNever } from 'type-fest';
 
-import type { PrismaFactory } from './prisma.factory.js';
+import type { ExtendedPrismaClient } from './prisma.factory.js';
 
 export type PrismaClientLike = PrismaClient & {
   [key: string]: any;
 };
-
-export type RuntimePrismaClient = Simplify<ReturnType<PrismaFactory['createClient']>>;
 
 export type PrismaModelName = IfNever<Prisma.ModelName, string, Prisma.ModelName>;
 
@@ -25,4 +23,4 @@ export type PrismaModelWhereInputMap = {
 };
 
 export type Model<T extends PrismaModelName> =
-  RuntimePrismaClient extends SingleKeyMap<`${Uncapitalize<T>}`, infer U> ? U : never;
+  ExtendedPrismaClient extends SingleKeyMap<`${Uncapitalize<T>}`, infer U> ? U : never;
