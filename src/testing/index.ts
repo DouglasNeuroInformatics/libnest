41114d4878eb2aefe8845c4c86a81cf6b5448ffc
joshunrau <contact@joshuaunrau.com>
2025-03-16 21:04:19 -0400

    refactor: helpers

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -2,7 +2,7 @@
  * @module testing
  */
 
-export type { EndToEndFixtures } from './extensions/e2e.js';
-export { e2e } from './extensions/e2e.js';
 export { MockFactory } from './factories/mock.factory.js';
 export type { MockedInstance } from './factories/mock.factory.js';
+export type { EndToEndContext } from './helpers/e2e.js';
+export { e2e } from './helpers/e2e.js';
