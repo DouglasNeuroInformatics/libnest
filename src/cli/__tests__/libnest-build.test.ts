acc034b754693d63b4e3196a8c61c2b06efd920e
joshunrau <contact@joshuaunrau.com>
2025-03-17 17:56:35 -0400

    test: update

--- src/cli/__tests__/libnest-build.test.ts
+++ src/cli/__tests__/libnest-build.test.ts
@@ -16,4 +16,21 @@ describe('libnest-build', () => {
     expect(result).toMatchObject({ exitCode: 0 });
     expect(process.stdout.write).toHaveBeenCalledWith(expect.stringContaining('Usage: libnest-build'));
   });
+  it('should set the action', async () => {
+    const action = vi.spyOn(Command.prototype, 'action');
+    await exec(['--help']);
+    const callback = action.mock.lastCall![0];
+    expect(callback).toBeTypeOf('function');
+  });
+  it('should throw an error if LIBNEST_CONFIG_FILEPATH is not defined', async () => {
+    const action = vi.spyOn(Command.prototype, 'action');
+    await exec(['--help']);
+    const callback = action.mock.lastCall![0];
+    vi.spyOn(process.env as any, 'LIBNEST_CONFIG_FILEPATH', 'get').mockReturnValueOnce(undefined);
+    await expect(() => (callback as any)()).rejects.toThrow(
+      expect.objectContaining({
+        exitCode: 1
+      })
+    );
+  });
 });
