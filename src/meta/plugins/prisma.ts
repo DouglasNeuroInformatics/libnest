cc68c991d1faa67779a562d7aaec1d8359705a7a
joshunrau <contact@joshuaunrau.com>
2025-03-19 16:29:26 -0400

    fix: inject query path at build

--- src/meta/plugins/prisma.ts
+++ src/meta/plugins/prisma.ts
@@ -12,28 +12,24 @@ const { getEnginesPath } = require('@prisma/engines') as typeof import('@prisma/
 export function prismaPlugin(): Plugin {
   return {
     name: 'prisma',
-    setup(build) {
+    setup: async (build) => {
       const options = build.initialOptions;
       const outdir = options.outfile ? path.dirname(options.outfile) : options.outdir!;
-      build.onEnd(async () => {
-        const binaryTarget = await getBinaryTargetForCurrentPlatform();
-        const enginesDir = getEnginesPath();
-        const pattern = new RegExp(`^libquery_engine-${binaryTarget}.*.node$`);
-        const engineFile = await fs.readdir(enginesDir).then((files) => {
-          return files.find((filename) => filename.match(pattern));
-        });
-        if (!engineFile) {
-          return {
-            errors: [
-              {
-                text: `Failed to find prisma query engine for target '${binaryTarget}' in directory '${enginesDir}'`
-              }
-            ]
-          };
-        }
 
-        await fs.copyFile(path.join(enginesDir, engineFile), path.join(outdir, engineFile));
-        return;
+      const binaryTarget = await getBinaryTargetForCurrentPlatform();
+      const enginesDir = getEnginesPath();
+      const pattern = new RegExp(`^libquery_engine-${binaryTarget}.*.node$`);
+      const engineFile = await fs.readdir(enginesDir).then((files) => {
+        return files.find((filename) => filename.match(pattern));
+      });
+      if (!engineFile) {
+        throw new Error(`Failed to find prisma query engine for target '${binaryTarget}' in directory '${enginesDir}'`);
+      }
+
+      build.initialOptions.define!['process.env.PRISMA_QUERY_ENGINE_LIBRARY'] = `"./${engineFile}"`;
+
+      build.onEnd(async () => {
+        return fs.copyFile(path.join(enginesDir, engineFile), path.join(outdir, engineFile));
       });
     }
   };
