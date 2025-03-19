0a88a8f376b9adca3568fa896d164f406f78f43e
joshunrau <contact@joshuaunrau.com>
2025-03-19 12:10:30 -0400

    refactor: move swc plugin

---
+++ src/meta/esbuild-plugins/swc.ts
@@ -0,0 +1,37 @@
+import * as fs from 'node:fs/promises';
+
+import { transform } from '@swc/core';
+import type { Plugin } from 'esbuild';
+
+export const swcPlugin = (): Plugin => {
+  return {
+    name: 'esbuild-plugin-swc',
+    setup: (build) => {
+      build.onLoad({ filter: /\.(ts)$/ }, async (args) => {
+        const code = await fs.readFile(args.path, 'utf-8');
+        const result = await transform(code, {
+          filename: args.path,
+          isModule: true,
+          jsc: {
+            parser: {
+              decorators: true,
+              syntax: 'typescript'
+            },
+            target: 'esnext',
+            transform: {
+              decoratorMetadata: true,
+              legacyDecorator: true
+            }
+          },
+          module: {
+            type: 'es6'
+          }
+        });
+        return {
+          contents: result.code,
+          loader: 'js'
+        };
+      });
+    }
+  };
+};
