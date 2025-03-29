9a44ec79522dd9539dd3f4d28aa94dc1fa6f3faa
joshunrau <contact@joshuaunrau.com>
2025-03-29 11:38:37 -0400

    refactor: add parseErrorLike method

--- src/modules/logging/json.logger.ts
+++ src/modules/logging/json.logger.ts
@@ -7,6 +7,7 @@ import type { LoggerService, LogLevel } from '@nestjs/common';
 import chalk from 'chalk';
 import type { ColorName } from 'chalk';
 import { isErrorLike, serializeError } from 'serialize-error';
+import type { ErrorLike } from 'serialize-error';
 
 import { LOGGING_MODULE_OPTIONS_TOKEN } from './logging.config.js';
 
@@ -139,7 +140,7 @@ export class JSONLogger implements JSONLoggerType, LoggerService {
     messages.forEach((message) => {
       const output: { [key: string]: unknown } = {};
       if (isErrorLike(message)) {
-        output.error = serializeError(message);
+        output.error = this.parseErrorLike(message);
       } else if (isPlainObject(message)) {
         Object.assign(output, message);
       } else {
@@ -154,5 +155,9 @@ export class JSONLogger implements JSONLoggerType, LoggerService {
       const content = format(JSON.stringify(output, null, 2) + '\n');
       process[options.file].write(content);
     });
+  }
+
+  private parseErrorLike(error: ErrorLike): { [key: string]: unknown } {
+    return serializeError(error);
   }
 }
