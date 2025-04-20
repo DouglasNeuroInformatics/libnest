c732e6d41682008a240466a69e1ef5097916b5aa
joshunrau <contact@joshuaunrau.com>
2025-04-20 14:48:16 -0400

    feat: implement integrationTestSuite

--- src/testing/helpers/integration.ts
+++ src/testing/helpers/integration.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
-import { afterAll, beforeAll, expect, suite } from 'vitest';
+import type { Class } from 'type-fest';
+import { expect, suite } from 'vitest';
 import type { ExpectStatic, TestAPI } from 'vitest';
 
 import { configureApp } from '../../app/app.utils.js';
@@ -11,11 +12,6 @@ import type { ImportedModule } from '../../app/app.base.js';
 import type { TestAgent } from './types.js';
 
 type IntegrationTestFactory = (ctx: { api: TestAgent; expect: ExpectStatic; it: TestAPI; test: TestAPI }) => void;
-
-type IntegrationTestSuite = {
-  api: TestAgent;
-  describe: (name: string, fn: IntegrationTestFactory) => void;
-};
 
 @Module({})
 export class IntegrationTestModule {
@@ -42,37 +38,27 @@ export class IntegrationTestModule {
   }
 }
 
-export function createIntegrationTestSuite(module: ImportedModule): IntegrationTestSuite {
-  let api: TestAgent;
+export function integrationTestSuite(module: Class<any>, fn: IntegrationTestFactory): void {
+  let app: NestExpressApplication;
+  const api = {} as TestAgent;
 
-  const describe = (name: string, fn: IntegrationTestFactory): void => {
-    suite(name, (test) => {
-      let app: NestExpressApplication;
+  const collector = suite(module.name, (test) => fn({ api, expect, it: test, test }));
 
-      beforeAll(async () => {
-        const moduleRef = await IntegrationTestModule.for(module);
-        app = moduleRef.createNestApplication({
-          logger: false
-        });
-
-        await configureApp(app);
-        await app.init();
-        api = request.agent(app.getHttpServer());
-      });
-
-      afterAll(async () => {
-        if (app) {
-          await app.close();
-          app.flushLogs();
-        }
-      });
-
-      fn({ api, expect, it: test, test });
+  collector.on('beforeAll', async () => {
+    const moduleRef = await IntegrationTestModule.for(module);
+    app = moduleRef.createNestApplication({
+      logger: false
     });
-  };
+    await configureApp(app);
+    await app.init();
+    const agent = request.agent(app.getHttpServer());
+    Object.setPrototypeOf(api, agent);
+  });
 
-  return {
-    api: api!,
-    describe
-  };
+  collector.on('afterAll', async () => {
+    if (app) {
+      await app.close();
+      app.flushLogs();
+    }
+  });
 }
--- src/testing/index.ts
+++ src/testing/index.ts
@@ -6,4 +6,4 @@ export { MockFactory } from './factories/mock.factory.js';
 export type { MockedInstance } from './factories/mock.factory.js';
 export type { EndToEndContext } from './helpers/e2e.js';
 export { e2e } from './helpers/e2e.js';
-export { createIntegrationTestSuite, IntegrationTestModule } from './helpers/integration.js';
+export { IntegrationTestModule, integrationTestSuite } from './helpers/integration.js';
