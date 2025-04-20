136b3491a23c34359859d4f1813e9a108b01ef5f
joshunrau <contact@joshuaunrau.com>
2025-04-20 13:38:45 -0400

    feat: add createIntegrationTestSuite

--- src/testing/helpers/integration.ts
+++ src/testing/helpers/integration.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { afterAll, beforeAll, expect, suite } from 'vitest';
+import type { ExpectStatic, TestAPI } from 'vitest';
+
+import { configureApp } from '../../app/app.utils.js';
 
 import type { ImportedModule } from '../../app/app.base.js';
+import type { TestAgent } from './types.js';
+
+type IntegrationTestFactory = (ctx: { api: TestAgent; expect: ExpectStatic; it: TestAPI; test: TestAPI }) => void;
+
+type IntegrationTestSuite = {
+  api: TestAgent;
+  describe: (name: string, fn: IntegrationTestFactory) => void;
+};
 
 @Module({})
 export class IntegrationTestModule {
@@ -26,4 +40,39 @@ export class IntegrationTestModule {
       ]
     }).compile();
   }
+}
+
+export function createIntegrationTestSuite(module: ImportedModule): IntegrationTestSuite {
+  let api: TestAgent;
+
+  const describe = (name: string, fn: IntegrationTestFactory): void => {
+    suite(name, (test) => {
+      let app: NestExpressApplication;
+
+      beforeAll(async () => {
+        const moduleRef = await IntegrationTestModule.for(module);
+        app = moduleRef.createNestApplication({
+          logger: false
+        });
+
+        await configureApp(app);
+        await app.init();
+        api = request.agent(app.getHttpServer());
+      });
+
+      afterAll(async () => {
+        if (app) {
+          await app.close();
+          app.flushLogs();
+        }
+      });
+
+      fn({ api, expect, it: test, test });
+    });
+  };
+
+  return {
+    api: api!,
+    describe
+  };
 }
