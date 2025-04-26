162c3a60d5b5bdb8c4c9cdc9b1e8fb843e397c5f
joshunrau <contact@joshuaunrau.com>
2025-04-26 15:27:34 -0400

    chore: add counter

--- example/ui/ui.service.tsx
+++ example/ui/ui.service.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+import type { JSX } from 'react';
+
 import { Injectable } from '@nestjs/common';
 
 import { JSXService } from '../../src/index.js';
+
+const UI = (): JSX.Element => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Current Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+    </div>
+  );
+};
 
 @Injectable()
 export class UIService {
   constructor(private readonly jsxService: JSXService) {}
 
   async render(): Promise<string> {
-    return this.jsxService.render(<h1>Hello World</h1>);
+    return this.jsxService.render(<UI />);
   }
 }
