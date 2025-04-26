97a2182195c299531c006d9a0daba4e921e9e2fc
joshunrau <contact@joshuaunrau.com>
2025-04-26 17:35:48 -0400

    test: add jsx service test

--- src/modules/jsx/__tests__/jsx.service.test.tsx
+++ src/modules/jsx/__tests__/jsx.service.test.tsx
@@ -1,17 +1,43 @@
-import { beforeAll, describe, expect, it } from 'vitest';
+import * as path from 'node:path';
+import { Writable } from 'node:stream';
+
+import { Document, Window } from 'happy-dom';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { JSXService } from '../jsx.service.js';
 
 describe('JSXService', () => {
   let jsxService: JSXService;
 
+  let document: Document;
+  let window: Window;
+
   beforeAll(() => {
     jsxService = new JSXService();
+    window = new Window({
+      innerHeight: 768,
+      innerWidth: 1024,
+      url: 'http://localhost:5500'
+    });
+    document = window.document;
+  });
+
+  afterAll(async () => {
+    await window.happyDOM.close();
   });
 
   describe('render', () => {
     it('should render hello world', async () => {
-      await expect(jsxService.render(<h1>Hello World</h1>)).resolves.toBe('<h1>Hello World</h1>');
+      let data = '';
+      const response = new Writable({
+        write(chunk, _encoding, callback) {
+          data += chunk.toString();
+          callback();
+        }
+      });
+      await jsxService.render(response, path.resolve(import.meta.dirname, 'components/Root.tsx'));
+      document.write(data);
+      expect(document.title).toBe('Counter');
     });
   });
 });
