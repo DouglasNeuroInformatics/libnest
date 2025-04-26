52608ac8595c83c99ab9211546abc913753fb265
joshunrau <contact@joshuaunrau.com>
2025-04-26 18:06:09 -0400

    test: add test for interactivity

--- src/modules/jsx/__tests__/jsx.service.test.tsx
+++ src/modules/jsx/__tests__/jsx.service.test.tsx
@@ -1,7 +1,7 @@
 import * as path from 'node:path';
 import { Writable } from 'node:stream';
 
-import { Document, Window } from 'happy-dom';
+import { Document, HTMLElement, Window } from 'happy-dom';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { JSXService } from '../jsx.service.js';
@@ -19,7 +19,7 @@ describe('JSXService', () => {
       innerWidth: 1024,
       url: 'http://localhost:5500'
     });
-    document = window.document;
+    document = window.document as any;
   });
 
   afterAll(async () => {
@@ -27,7 +27,7 @@ describe('JSXService', () => {
   });
 
   describe('render', () => {
-    it('should render hello world', async () => {
+    it('should successfully pipe the generated html to the response', async () => {
       let data = '';
       const response = new Writable({
         write(chunk, _encoding, callback) {
@@ -37,7 +37,18 @@ describe('JSXService', () => {
       });
       await jsxService.render(response, path.resolve(import.meta.dirname, 'components/Root.tsx'));
       document.write(data);
-      expect(document.title).toBe('Counter');
+      await window.happyDOM.waitUntilComplete();
+    });
+
+    it('should generate the correct html', async () => {
+      const counterHeading = document.getElementById('counter-heading') as HTMLElement;
+      const counterButton = document.getElementById('counter-button') as HTMLElement;
+      expect(counterHeading).toBeDefined();
+      expect(counterButton).toBeDefined();
+      expect(counterHeading.innerText).toBe('Current Count: 0');
+      counterButton.click();
+      await window.happyDOM.waitUntilComplete();
+      expect(counterHeading.innerText).toBe('Current Count: 1');
     });
   });
 });
