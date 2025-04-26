694dcf00058e628dd04930c28b77f8af7003bf1e
joshunrau <contact@joshuaunrau.com>
2025-04-26 18:06:18 -0400

    test: add ids

--- src/modules/jsx/__tests__/components/Counter.tsx
+++ src/modules/jsx/__tests__/components/Counter.tsx
@@ -4,8 +4,10 @@ export const Counter: React.FC<{}> = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Current Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <h1 id="counter-heading">Current Count: {count}</h1>
+      <button id="counter-button" onClick={() => setCount(count + 1)}>
+        Increment Count
+      </button>
     </div>
   );
 };
