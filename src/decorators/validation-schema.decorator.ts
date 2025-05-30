991ee5fd8977188549c35f14adb822f302e6df82
joshunrau <contact@joshuaunrau.com>
2025-05-30 14:13:56 -0400

    refactor: implement zod v4 in decorators

--- src/decorators/validation-schema.decorator.ts
+++ src/decorators/validation-schema.decorator.ts
@@ -1,20 +1,22 @@
-import type { Class } from 'type-fest';
-import { z } from 'zod';
+import type { Class, OmitIndexSignature } from 'type-fest';
+import { z } from 'zod/v4';
 
 import { applyValidationSchema } from '../utils/validation.utils.js';
 
-export function ValidationSchema<T extends z.ZodType<{ [key: string]: any }>>(
+export function ValidationSchema<T extends z.ZodObject>(
   schema: T
-): (target: Class<z.output<T>>) => void;
+): (target: Class<OmitIndexSignature<T['_output']>>) => void;
 
-export function ValidationSchema<T extends z.ZodRawShape>(shape: T): (target: Class<z.output<z.ZodObject<T>>>) => void;
+export function ValidationSchema<T extends z.ZodRawShape>(
+  shape: T
+): (target: Class<OmitIndexSignature<z.ZodObject<T>['_output']>>) => void;
 
 /**
  * Decorator to define the Zod validation schema for DTO classes
  * @param schema The Zod schema to use for validation.
  * @returns A class decorator that applies the schema.
  */
-export function ValidationSchema(shapeOrSchema: z.ZodRawShape | z.ZodType<{ [key: string]: any }>) {
+export function ValidationSchema(shapeOrSchema: z.ZodObject | z.ZodRawShape) {
   const schema = shapeOrSchema instanceof z.ZodType ? shapeOrSchema : z.object(shapeOrSchema);
   return (target: Class<any>): void => {
     applyValidationSchema(target, schema);
