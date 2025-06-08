c3aec0553025862faa480524bae16df0fae310b0
joshunrau <contact@joshuaunrau.com>
2025-06-08 17:33:10 -0400

    fix: export createAppAbility for testing

--- src/modules/auth/ability.factory.ts
+++ src/modules/auth/ability.factory.ts
@@ -1,21 +1,15 @@
-import { AbilityBuilder, detectSubjectType } from '@casl/ability';
+import { AbilityBuilder } from '@casl/ability';
 import { createPrismaAbility } from '@casl/prisma';
 import { Inject, Injectable } from '@nestjs/common';
 
 import { LoggingService } from '../logging/logging.service.js';
+import { createAppAbility, detectAppSubject } from './ability.utils.js';
 import { DEFINE_ABILITY_TOKEN } from './auth.config.js';
 
-import type { AppAbilities, AppAbility, DefineAbility, Permission } from './auth.config.js';
+import type { AppAbility, DefineAbility, Permission } from './auth.config.js';
 
 @Injectable()
 export class AbilityFactory {
-  private detectSubjectType = (obj: { [key: string]: unknown }): string => {
-    if (typeof obj.__modelName === 'string') {
-      return obj.__modelName;
-    }
-    return detectSubjectType(obj);
-  };
-
   constructor(
     private readonly loggingService: LoggingService,
     @Inject(DEFINE_ABILITY_TOKEN) private readonly defineAbility?: DefineAbility
@@ -32,7 +26,7 @@ export class AbilityFactory {
       this.defineAbility(abilityBuilder, payload, metadata);
     }
     return abilityBuilder.build({
-      detectSubjectType: this.detectSubjectType
+      detectSubjectType: detectAppSubject
     });
   }
 
@@ -41,8 +35,6 @@ export class AbilityFactory {
       message: 'Creating Ability From Permissions',
       permissions
     });
-    return createPrismaAbility<AppAbilities>(permissions, {
-      detectSubjectType: this.detectSubjectType
-    });
+    return createAppAbility(permissions);
   }
 }
