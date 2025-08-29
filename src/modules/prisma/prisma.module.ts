235b28e24ac49458e1352c9b4600548986c6ec37
joshunrau <contact@joshuaunrau.com>
2025-08-29 14:49:05 -0400

    chore: update

--- src/modules/prisma/prisma.module.ts
+++ src/modules/prisma/prisma.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import type { ConfigurableModuleAsyncOptions, DynamicModule, FactoryProvider } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
 
@@ -9,7 +9,21 @@ import { getModelKey, getModelToken } from './prisma.utils.js';
 import type { PrismaModuleOptions } from './prisma.config.js';
 import type { PrismaClientLike } from './prisma.types.js';
 
+const MODEL_PROVIDERS: FactoryProvider[] = Object.keys(Prisma.ModelName).map((modelName) => {
+  return {
+    inject: [PRISMA_CLIENT_TOKEN],
+    provide: getModelToken(modelName),
+    useFactory: (prismaClient: PrismaClientLike): unknown => {
+      return prismaClient[getModelKey(modelName)];
+    }
+  };
+});
+
+const MODEL_TOKENS = MODEL_PROVIDERS.map((provider) => provider.provide);
+
+@Global()
 @Module({
+  exports: [PRISMA_CLIENT_TOKEN, PrismaService, ...MODEL_TOKENS],
   providers: [
     {
       inject: [PRISMA_MODULE_OPTIONS_TOKEN],
@@ -19,15 +33,7 @@ import type { PrismaClientLike } from './prisma.types.js';
       }
     },
     PrismaService,
-    ...Object.keys(Prisma.ModelName).map((modelName) => {
-      return {
-        inject: [PRISMA_CLIENT_TOKEN],
-        provide: getModelToken(modelName),
-        useFactory: (prismaClient: PrismaClientLike): unknown => {
-          return prismaClient[getModelKey(modelName)];
-        }
-      } satisfies FactoryProvider;
-    })
+    ...MODEL_PROVIDERS
   ]
 })
 export class PrismaModule extends ConfigurablePrismaModule {
