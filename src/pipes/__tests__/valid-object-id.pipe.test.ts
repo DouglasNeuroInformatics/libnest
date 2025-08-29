909f1dbd0c5ad20783aa01a695778331dab81fc3
joshunrau <contact@joshuaunrau.com>
2025-08-29 13:51:02 -0400

    refactor: remove dependency on mongodb

--- src/pipes/__tests__/valid-object-id.pipe.test.ts
+++ src/pipes/__tests__/valid-object-id.pipe.test.ts
@@ -1,5 +1,4 @@
 import { BadRequestException } from '@nestjs/common';
-import { ObjectId } from 'mongodb';
 import { describe, expect, it } from 'vitest';
 
 import { ValidObjectIdPipe } from '../valid-object-id.pipe.js';
@@ -7,7 +6,7 @@ import { ValidObjectIdPipe } from '../valid-object-id.pipe.js';
 describe('ValidObjectIdPipe', () => {
   it('should return the input value if it is a valid object id', () => {
     const pipe = new ValidObjectIdPipe();
-    const value = new ObjectId().toString();
+    const value = '68b1e844010c69adcc711123';
     expect(pipe.transform(value)).toEqual(value);
   });
   it('should throw BadRequestException if validation fails', () => {
--- src/pipes/valid-object-id.pipe.ts
+++ src/pipes/valid-object-id.pipe.ts
@@ -1,11 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import type { PipeTransform } from '@nestjs/common';
-import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class ValidObjectIdPipe implements PipeTransform<string> {
   transform(value: unknown): string {
-    if (!(typeof value === 'string' && ObjectId.isValid(value))) {
+    if (!(typeof value === 'string' && /^[0-9a-f]{24}$/.test(value))) {
       throw new BadRequestException(`Invalid ObjectID: ${String(value)}`);
     }
     return value;
