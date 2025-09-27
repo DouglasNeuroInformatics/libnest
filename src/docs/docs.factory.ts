14cbd71086fa73ee61ed5d4991b9c3d8b8f2ccaf
joshunrau <contact@joshuaunrau.com>
2025-09-27 09:25:17 -0400

    refactor: implement fastify in docs

--- src/docs/docs.factory.ts
+++ src/docs/docs.factory.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
-import type { NestExpressApplication } from '@nestjs/platform-express';
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import type { OpenAPIObject } from '@nestjs/swagger';
 
@@ -29,24 +29,24 @@ export type DocsConfig = {
 };
 
 export class DocsFactory {
-  static async configureDocs(app: NestExpressApplication, config: DocsConfig): Promise<void> {
+  static async configureDocs(app: NestFastifyApplication, config: DocsConfig): Promise<void> {
     const document = this.createDocs(app, config);
     const httpAdapter = app.getHttpAdapter().getInstance();
     const specUrl = config.path.endsWith('/') ? config.path + 'spec.json' : config.path + '/spec.json';
-    httpAdapter.get(specUrl, (_, res) => {
-      res.send(document);
+    httpAdapter.get(specUrl, (_, reply) => {
+      reply.send(document);
     });
     let html = await fs.readFile(path.resolve(import.meta.dirname, 'assets/index.html'), 'utf-8');
     html = html.replace('{{TITLE}}', config.title);
     html = html.replace('{{SPEC_URL}}', specUrl);
-    httpAdapter.get(config.path, (_, res) => {
-      res.set('Content-Type', 'text/html');
-      res.send(html);
+    httpAdapter.get(config.path, (_, reply) => {
+      reply.type('text/html');
+      reply.send(html);
     });
   }
 
   private static createDocs(
-    app: NestExpressApplication,
+    app: NestFastifyApplication,
     { contact, description, externalDoc, license, tags, title, version }: DocsConfig
   ): OpenAPIObject {
     const documentBuilder = new DocumentBuilder();
