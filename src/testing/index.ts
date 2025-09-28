42f868278d2eee17c49ee1536354a94eb0ee8aa6
joshunrau <contact@joshuaunrau.com>
2025-09-28 10:30:43 -0400

    chore: export MockPrismaClient

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -5,3 +5,4 @@
 export { MockFactory } from './factories/mock.factory.js';
 export type { MockedInstance } from './factories/mock.factory.js';
 export { e2e } from './helpers/e2e.js';
+export { MockPrismaClient } from './mocks/prisma.client.mock.js';
