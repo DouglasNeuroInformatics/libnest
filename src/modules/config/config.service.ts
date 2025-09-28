c6d869b3b63bd63992aaa134b7304bdab9dea52a
joshunrau <contact@joshuaunrau.com>
2025-09-28 11:08:51 -0400

    feat: allow custom env for testing in config service

--- src/modules/config/config.service.ts
+++ src/modules/config/config.service.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@nestjs/common';
 import type { UserTypes } from '../../user-config.js';
 
 @Injectable()
-export class ConfigService {
+export class ConfigService<TEnv extends { [key: string]: unknown } = UserTypes.Env> {
   /** @hidden */
-  constructor(private readonly envConfig: UserTypes.Env) {}
+  constructor(private readonly envConfig: TEnv) {}
 
   /**
    * Retrieves a configuration value by key.
    * @param key The key of the configuration value to retrieve.
    * @returns The configuration value
    */
-  get<TKey extends Extract<keyof UserTypes.Env, string>>(key: TKey): UserTypes.Env[TKey] {
+  get<TKey extends Extract<keyof TEnv, string>>(key: TKey): TEnv[TKey] {
     return this.envConfig[key];
   }
 
@@ -22,11 +22,11 @@ export class ConfigService {
    * @returns The configuration value.
    * @throws Error if the configuration value is undefined.
    */
-  getOrThrow<TKey extends Extract<keyof UserTypes.Env, string>>(key: TKey): Exclude<UserTypes.Env[TKey], undefined> {
+  getOrThrow<TKey extends Extract<keyof TEnv, string>>(key: TKey): Exclude<TEnv[TKey], undefined> {
     const value = this.envConfig[key];
     if (value === undefined) {
       throw new Error(`Property '${key}' is undefined`);
     }
-    return value as Exclude<UserTypes.Env[TKey], undefined>;
+    return value as Exclude<TEnv[TKey], undefined>;
   }
 }
