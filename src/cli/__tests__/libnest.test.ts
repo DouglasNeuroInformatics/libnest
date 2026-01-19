813802e1be1765446e3a46346f18c212a0795e96
joshunrau <contact@joshuaunrau.com>
2026-01-19 13:38:23 -0500

    test: refactor all cli tests for vitest v4

--- src/cli/__tests__/libnest.test.ts
+++ src/cli/__tests__/libnest.test.ts
@@ -2,7 +2,7 @@ import { Command } from 'commander';
 import { err, ok } from 'neverthrow';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
-import { createExec, process } from '../../testing/helpers/cli.js';
+import { CommandRunner } from '../../testing/helpers/cli.js';
 
 const { resolveAbsoluteImportPath, resolveAbsolutePath } = vi.hoisted(() => ({
   resolveAbsoluteImportPath: vi.fn(),
@@ -14,7 +14,7 @@ vi.mock('../../meta/resolve.js', () => ({
   resolveAbsolutePath
 }));
 
-const exec = createExec({
+const command = new CommandRunner({
   entry: '../libnest.js',
   root: import.meta.dirname
 });
@@ -26,39 +26,41 @@ describe('libnest', () => {
 
   it('should output help', async () => {
     const parseAsync = vi.spyOn(Command.prototype, 'parseAsync');
-    const result = await exec(['--help']);
+    const result = await command.run(['--help']);
     expect(parseAsync).toHaveBeenCalledExactlyOnceWith(['node', '../libnest.js', '--help']);
-    expect(result).toMatchObject({ exitCode: 0 });
-    expect(process.stdout.write).toHaveBeenCalledWith(
-      expect.stringContaining('Usage: @douglasneuroinformatics/libnest')
-    );
+    expect(result.error?.exitCode).toBe(0);
+    expect(result.stdout).toContain('Usage: @douglasneuroinformatics/libnest');
   });
+
   it('should throw an InvalidArgumentError if the config path cannot be resolved', async () => {
-    process.cwd.mockReturnValueOnce('/app');
     const parseAsync = vi.spyOn(Command.prototype, 'parseAsync');
     resolveAbsoluteImportPath.mockReturnValueOnce(err('Failed'));
-    const result = await exec(['-c', 'libnest.config.ts']);
+    const result = await command.run(['-c', 'libnest.config.ts'], {
+      cwd: '/app'
+    });
     expect(parseAsync).toHaveBeenCalledExactlyOnceWith(['node', '../libnest.js', '-c', 'libnest.config.ts']);
     expect(resolveAbsoluteImportPath).toHaveBeenLastCalledWith('libnest.config.ts', '/app');
-    expect(result).toMatchObject({ code: 'commander.invalidArgument', exitCode: 1 });
+    expect(result.error).toMatchObject({ code: 'commander.invalidArgument', exitCode: 1 });
   });
+
   it('should source the env file', async () => {
     const hook = vi.spyOn(Command.prototype, 'hook');
     resolveAbsoluteImportPath.mockReturnValueOnce(ok('/root/path/to/file.js'));
     resolveAbsolutePath.mockReturnValueOnce(ok('/root/path/to/file.js'));
-    await exec(['-c', 'libnest.config.ts', '--env-file', '.env']);
+    const result = await command.run(['-c', 'libnest.config.ts', '--env-file', '.env']);
     expect(hook).toHaveBeenCalledExactlyOnceWith('preSubcommand', expect.any(Function));
     const callback = hook.mock.lastCall![1];
     const getOptionValue = vi.fn();
     getOptionValue.mockReturnValueOnce(['.env']);
     await callback({ getOptionValue } as any, null!);
     expect(getOptionValue).toHaveBeenCalled();
-    expect(process.loadEnvFile).toHaveBeenCalledExactlyOnceWith('.env');
+    expect(result.mocks.process.loadEnvFile).toHaveBeenCalledExactlyOnceWith('.env');
   });
+
   it('should pass the resolved config file to the subcommand', async () => {
     const hook = vi.spyOn(Command.prototype, 'hook');
     resolveAbsoluteImportPath.mockReturnValueOnce(ok('/root/path/to/file.js'));
-    await exec(['-c', 'libnest.config.ts']);
+    await command.run(['-c', 'libnest.config.ts']);
     expect(hook).toHaveBeenCalledExactlyOnceWith('preSubcommand', expect.any(Function));
     const callback = hook.mock.lastCall![1];
     const getOptionValue = vi.fn();
