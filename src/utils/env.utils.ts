8c3ca87df190713c682c12855d76cdd8a767e7d9
joshunrau <contact@joshuaunrau.com>
2026-04-22 12:48:32 -0400

    chore: update typescript to v6

--- src/utils/env.utils.ts
+++ src/utils/env.utils.ts
@@ -21,6 +21,6 @@ export function parseEnv<TSchema extends BaseEnvSchema = BaseEnvSchema>(schema: 
       }
     });
   }
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+
   return envConfigResult.value;
 }
